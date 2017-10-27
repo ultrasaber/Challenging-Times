0a47c8f33fc120d85b6d1c29f98dd8621a009772
ultrasaber <saberthesaber@gmail.com>
2017-10-27 20:44:10 +0000

    Part One: Routing Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  root to: 'pages#home', as: :home
+  get '/about_us', to: 'pages#about', as: :about
+
+  get 'pages/home'
+
+  get 'pages/about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
