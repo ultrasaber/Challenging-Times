883fe59addfe1474fab5685ed3212f18c17871e9
ultrasaber <saberthesaber@gmail.com>
2017-10-28 22:35:52 +0000

    Part Two: Routing Lizards

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  root to: 'pages#home', as: :home
-  get '/about_us', to: 'pages#about', as: :about
+  root to: 'pages#home', as: 'home'
+  get '/about_us', to: 'pages#about', as: 'about'
+  get 'lizards/:number', to: 'pages#lizards', as: 'lizards'
+  
 
   get 'pages/home'
 
